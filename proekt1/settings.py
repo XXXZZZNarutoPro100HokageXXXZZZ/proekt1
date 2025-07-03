74f8e93305b6564800863e151420b4a7f289d31d
KORKA1692 <131654584+XXXZZZNarutoPro100HokageXXXZZZ@users.noreply.github.com>
2025-07-03 11:38:18 +0300

    Update settings.py

--- proekt1/settings.py
+++ proekt1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8q+(i*7b2-t4-c6&1z7#b$i^^)%jhi*^w$=+z&h+jilzz6kvea'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
